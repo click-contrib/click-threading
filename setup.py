99c47e64c7897f5a422e07d0112b42f0a06ac255
Hugo Barrera <hugo@barrera.io>
2021-05-30 22:11:27 +0200

    Unbreak master (#6)
    Co-authored-by: Jim Turner <git@turner.link>

--- setup.py
+++ setup.py
@@ -23,7 +23,12 @@ setup(
     install_requires=[
         'click>=5.0',
     ],
-    extras_require={
-        ':python_version < "3.2"': 'futures'
-    }
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
